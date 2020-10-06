989e33ef6b27eebb62ce7c912c789bd0d62afc40
christinak44 <kingcm41@hotmail.com>
2020-10-06 09:00:13 -0400

    trying to avoid repeating quote until all quotes have shown at least once

--- js/script.js
+++ js/script.js
@@ -12,7 +12,9 @@ project 1 - A Random Quote Generator
 ***/
 //Homer Simpson quotes from screenrant.com
 const quotes = [
-  {quote: 'It takes two to lie; One to lie, and one to listen.',
+  {
+    id: 1,
+    quote: 'It takes two to lie; One to lie, and one to listen.',
     source: 'Homer Simpson',
     // type: 'Real Talk ala Mr. Simpson',
     // citation: 'The Simpsons, Season 3, Ep 20',
@@ -20,36 +22,44 @@ const quotes = [
 
   },
 
-  {quote: "Facts are meaningless. You could use facts to prove anything that's even remotely true.",
-  source: 'Homer Simpson',
-  // type: 'Real Talk ala Mr. Simpson',
-  // citation: 'The Simpsons, Season 9, EP 8',
-  // year: 1997
+  {
+    id: 2,
+    quote: "Facts are meaningless. You could use facts to prove anything that's even remotely true.",
+    source: 'Homer Simpson',
+    // type: 'Real Talk ala Mr. Simpson',
+    // citation: 'The Simpsons, Season 9, EP 8',
+    // year: 1997
 
   },
 
-  {quote:  "I am so smart! I am so smart! S-M-R-T! I mean S-M-A-R-T!",
-  source: 'Homer Simpson',
-  // type: 'Motivation ala Mr. Simpson',
-  citation: 'The Simpsons, Season 5, Ep 3',
-  year: 1993
+  {
+    id: 3,
+    quote:  "I am so smart! I am so smart! S-M-R-T! I mean S-M-A-R-T!",
+    source: 'Homer Simpson',
+    // type: 'Motivation ala Mr. Simpson',
+    citation: 'The Simpsons, Season 5, Ep 3',
+    year: 1993
 
   },
 
-  {quote: "D'oh!",
-  source: 'Homer Simpson',
-  // type: 'Classic Mr. Simpson',
-  citation: 'The Simpsons',
-  // year: ''
+  {
+    id: 4,
+    quote: "D'oh!",
+    source: 'Homer Simpson',
+    // type: 'Classic Mr. Simpson',
+    citation: 'The Simpsons',
+    // year: ''
 
   },
   //simpsons.fandom.com/wiki
 
-  {quote: "All my life I've had one dream. To achieve my many goals. Mr. Burns has never given me a thumbs up or a 'way to be' or a 'you go girl!'",
-  source: 'Homer Simpson',
-  // type: 'Relatable Mr. Simpson',
-  // citation: 'The Simpsons, Season 14, EP 15',
-  year: 2003
+  {
+    id: 5,
+    quote: "All my life I've had one dream. To achieve my many goals. Mr. Burns has never given me a thumbs up or a 'way to be' or a 'you go girl!'",
+    source: 'Homer Simpson',
+    // type: 'Relatable Mr. Simpson',
+    // citation: 'The Simpsons, Season 14, EP 15',
+    year: 2003
   }
 ];
 // console.log(quotes);
@@ -69,32 +79,25 @@ function getRandomQuote() {
  * `printQuote` function
 ***/
 function printQuote() {
+  const seenQuotes = {};
   let quote = getRandomQuote();
-  let displayQuote = '';
-    if (quote.citation && quote.year) {
-      displayQuote =
-      `<p class="quote"> ${quote.quote} </p>
-      <p class="source"> ${quote.source}
-        <span class="citation"> ${quote.citation} </span>
-        <span class="year"> ${quote.year} </span>
-      </p>`
-  } else if (quote.citation && !quote.year) {
-    displayQuote =
-    `<p class="quote"> ${quote.quote} </p>
-    <p class="source"> ${quote.source}
-      <span class="citation"> ${quote.citation} </span>
-    </p>`
-  } else if (quote.year && !quote.citation) {
-  displayQuote =
-  `<p class="quote"> ${quote.quote} </p>
-  <p class="source"> ${quote.source}
-    <span class="year"> ${quote.year} </span>
-  </p>`
-  } else {
-    displayQuote =
-    `<p class="quote"> ${quote.quote} </p>
-  <p class="source"> ${quote.source} `
+  while(seenQuotes[quote.id]){
+    //Only add to seenQuotes if this quote ID isn't there
+    if(!seenQuotes[quote.id]){
+      seenQuotes[quote.id] = true;
+    } else {
+      //call getRandomQuote again if it's an already seen quote
+      quote = getRandomQuote();
+    }
   }
+  let displayQuote = `<p class="quote"> ${quote.quote} </p><p class="source"> ${quote.source}`;
+  if (quote.citation) {
+    displayQuote += `<span class="citation"> ${quote.citation} </span>`;
+  }
+  if (quote.year) {
+    displayQuote += `<span class="year"> ${quote.year} </span>`;
+  }
+  displayQuote += `</p>`;
   return document.getElementById('quote-box').innerHTML =  displayQuote;
 }
 
@@ -104,3 +107,4 @@ function printQuote() {
 ***/
 
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
+printQuote();
